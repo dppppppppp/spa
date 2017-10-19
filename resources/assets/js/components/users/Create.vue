94c25ee8d49057f0f11ab2a7a043691799eb06dc
dp~ <457509824@qq.com>
2017-10-19 10:48:28 +0800

    添加vee-validate前端验证

--- resources/assets/js/components/users/Create.vue
+++ resources/assets/js/components/users/Create.vue
@@ -8,39 +8,51 @@
                     <div class="panel-body">
                         <form class="form-horizontal" v-on:submit.prevent="onSubmit">
 
-                            <div class="form-group">
+                            <div class="form-group" :class="{'has-error':errors.has('name')}">
                                 <label for="name" class="col-md-4 control-label">用户名</label>
 
                                 <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control" name="name" value="" required
+                                    <input id="name"
+                                           v-validate="{ required: true, min:4 }"
+                                           type="text" class="form-control" name="name" value="" required
                                            autofocus v-model="name">
+                                    <span v-show="errors.has('name')" class="help-block">{{errors.first('name')}}</span>
                                 </div>
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group" :class="{'has-error':errors.has('email')}">
                                 <label for="email" class="col-md-4 control-label">邮箱</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" value="" required
+                                    <input id="email"
+                                           v-validate="{ required: true, email:true }"
+                                           type="email" class="form-control" name="email" value="" required
                                            v-model="email">
+                                    <span v-show="errors.has('email')" class="help-block">{{errors.first('email')}}</span>
                                 </div>
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group" :class="{'has-error':errors.has('password')}">
                                 <label for="password" class="col-md-4 control-label">密码</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required
+                                    <input id="password"
+                                           v-validate="{ required: true, min:6 }"
+                                           type="password" class="form-control" name="password" required
                                            v-model="password">
+                                    <span v-show="errors.has('password')" class="help-block">{{errors.first('password')}}</span>
                                 </div>
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group" :class="{'has-error':errors.has('password_confirmation')}">
                                 <label for="password-confirm" class="col-md-4 control-label">确认密码</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
+                                    <input id="password-confirm"
+                                           v-validate="{ required: true, confirmed:'password' }"
+                                           type="password" class="form-control"
                                            name="password_confirmation" required v-model="passwordConfirmation">
+                                    <span v-show="errors.has('password_confirmation')" class="help-block">{{errors.first('password_confirmation')}}</span>
                                 </div>
                             </div>
 
@@ -66,7 +78,7 @@
                 name: '',
                 email: '',
                 password: '',
-                passwordConfirmation:''
+                passwordConfirmation: ''
             }
         },
         methods: {
@@ -75,7 +87,7 @@
                     name: this.name,
                     email: this.email,
                     password: this.password,
-                    'password_confirmation':this.passwordConfirmation
+                    'password_confirmation': this.passwordConfirmation
                 };
                 axios.post('/api/register', postData).then(response => {
                     console.log(response);
